8f7fd713af96f40f20127a816550e889836e3a07
libin <bin.li@gaea.com>
2022-01-15 11:48:37 +0800

    Go编程模式：详解函数式选项模式

--- 220114_rpcjson/demo2/client/client.sh
+++ 220114_rpcjson/demo2/client/client.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 curl localhost:8888/jsonrpc -X POST \
-    --data '{"method":"HelloService.Length","params":["hello"],"id":0}'
+    --data '{"method":"HelloService.Length","params":["test str 666"],"id":0}'
