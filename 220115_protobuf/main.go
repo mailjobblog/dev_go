cfdb031b215bc7550110fc50c6af03ba3de495d4
libin <bin.li@gaea.com>
2022-01-15 15:43:41 +0800

    push

---
+++ 220115_protobuf/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/golang/protobuf/proto"
+	"golang/proto/pb"
+	"log"
+)
+
+func main() {
+
+	test := &pb.Student{
+		Name:   "geektutu",
+		Male:   true,
+		Scores: []int32{98, 85, 88},
+	}
+	data, err := proto.Marshal(test)
+	if err != nil {
+		log.Fatal("marshaling error: ", err)
+	}
+	newTest := &pb.Student{}
+	err = proto.Unmarshal(data, newTest)
+	if err != nil {
+		log.Fatal("unmarshaling error: ", err)
+	}
+	// Now test and newTest contain the same data.
+	if test.GetName() != newTest.GetName() {
+		log.Fatalf("data mismatch %q != %q", test.GetName(), newTest.GetName())
+	}
+}
