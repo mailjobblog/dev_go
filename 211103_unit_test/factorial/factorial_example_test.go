ea58df3eddbec164098f39122775d3a64501adbc
libin <bin.li@gaea.com>
2022-06-08 17:29:04 +0800

    1

---
+++ 211103_unit_test/factorial/factorial_example_test.go
@@ -0,0 +1,12 @@
+package factorial
+
+import (
+	"fmt"
+)
+
+// Example
+func ExampleFactorial() {
+	fmt.Println(Factorial(5))
+
+	// Output:&Fact{ret:120, nums:[]int{5,4,3,2,1}}
+}
--- 211103_unit_test/go.mod
+++ 211103_unit_test/go.mod
@@ -1,3 +1,8 @@
 module unit_test
 
 go 1.18
+
+require (
+	github.com/cweill/gotests v1.6.0 // indirect
+	golang.org/x/tools v0.0.0-20191109212701-97ad0ed33101 // indirect
+)
